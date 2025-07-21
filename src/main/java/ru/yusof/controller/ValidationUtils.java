1b717f7bfc9f058bb393f01dc4b7d9630198623a
YusofAlSalekh <y.alsalekh@gmail.com>
2025-07-21 21:37:18 +0200

    s09-t01

--- src/main/java/ru/yusof/controller/ValidationUtils.java
+++ src/main/java/ru/yusof/controller/ValidationUtils.java
@@ -4,28 +4,25 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class ValidationUtils {
-    protected static String validateNotBlankString(String paramName) {
+    protected static void validateNotBlankString(String paramName) {
         if (paramName == null || paramName.isBlank()) {
             throw new IllegalArgumentException("name is required");
         }
-        return paramName;
     }
 
-    protected static BigDecimal validatePositiveBigDecimal(BigDecimal value) {
+    protected static void validatePositiveBigDecimal(BigDecimal value) {
         if (value == null || value.signum() < 0) {
             throw new IllegalArgumentException("A positive number must be provided");
         }
-        return value;
     }
 
-    protected static Integer validatePositiveInteger(Integer value) {
+    protected static void validatePositiveInteger(Integer value) {
         if (value == null || value < 0) {
             throw new IllegalArgumentException("A positive number must be provided");
         }
-        return value;
     }
 
-    protected static List<Integer> validateIntegerList(List<Integer> values) {
+    protected static void validateIntegerList(List<Integer> values) {
         if (values == null || values.isEmpty()) {
             throw new IllegalArgumentException(" Values must be provided");
         }
@@ -33,6 +30,5 @@ public class ValidationUtils {
         for (Integer value : values) {
             validatePositiveInteger(value);
         }
-        return values;
     }
 }
